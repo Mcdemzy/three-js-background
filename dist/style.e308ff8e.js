1ddf264d25551031e786b73d42c507157f112adb
Adeyemo Ademola <adeyemoademola12@gmail.com>
2024-01-13 22:26:19 +0100

    Changed to "run dev" command

--- dist/style.e308ff8e.js
+++ dist/style.e308ff8e.js
@@ -172,7 +172,7 @@ module.exports = reloadCSS;
 var reloadCSS = require('_css_loader');
 module.hot.dispose(reloadCSS);
 module.hot.accept(reloadCSS);
-},{"C:\\Users\\user\\Desktop\\MCDEMZY\\Front End\\Github Projects\\three-js-background\\img\\bg1.avif":[["bg1.5b5cfb48.avif","img/bg1.avif"],"img/bg1.avif"],"_css_loader":"../../../../../AppData/Roaming/npm/node_modules/parcel-bundler/src/builtins/css-loader.js"}],"../../../../../AppData/Roaming/npm/node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
+},{"C:\\Users\\user\\Desktop\\MCDEMZY\\Front End\\Github Projects\\three-js-background\\img\\bg1.avif":[["bg1.5b5cfb48.avif","img/bg1.avif"],"img/bg1.avif"],"_css_loader":"../../../../../AppData/Roaming/npm/node_modules/parcel-bundler/src/builtins/css-loader.js"}],"node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
 var OldModule = module.bundle.Module;
@@ -197,7 +197,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "62410" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "62503" + '/');
   ws.onmessage = function (event) {
     checkedAssets = {};
     assetsToAccept = [];
@@ -341,5 +341,5 @@ function hmrAcceptRun(bundle, id) {
     return true;
   }
 }
-},{}]},{},["../../../../../AppData/Roaming/npm/node_modules/parcel-bundler/src/builtins/hmr-runtime.js"], null)
+},{}]},{},["node_modules/parcel-bundler/src/builtins/hmr-runtime.js"], null)
 //# sourceMappingURL=/style.e308ff8e.js.map
