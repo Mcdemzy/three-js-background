58ec490b99e4abad8defe4902e17d0b73f89c1bd
Adeyemo Ademola <adeyemoademola12@gmail.com>
2024-01-14 00:09:52 +0100

    Update

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 import * as THREE from "three";
 import "./style.css";
 import bg1 from "./img/bg1.avif";
-import bg2 from "./img/bg2.avif";
 
 const container = document.querySelector(".three_bg");
 const loader = new THREE.TextureLoader();
@@ -20,7 +19,6 @@ container.appendChild(renderer.domElement);
 
 const geometry = new THREE.PlaneGeometry(14, 8, 15, 9);
 const material = new THREE.MeshBasicMaterial({
-  //   color: 0xff0000,
   map: loader.load(bg1),
 });
 
@@ -32,14 +30,24 @@ const count = geometry.attributes.position.count;
 const clock = new THREE.Clock();
 
 function animate() {
-  const time = clock.getElapsedTime;
-  for (let i = 0; i < count; i++) {
-    const x = geometry.attributes.position.getX;
-    const y = geometry.attributes.position.getY;
+  const time = clock.getElapsedTime();
 
-    const anim1
+  for (let i = 0; i < count; i++) {
+    const x = geometry.attributes.position.getX(i);
+    const y = geometry.attributes.position.getY(i);
+
+    const anim1 = 0.25 * Math.sin(x + time * 0.7);
+    const anim2 = 0.35 * Math.sin(x * 1 + time * 0.7);
+    const anim3 = 0.1 * Math.sin(y * 15 + time * 0.7);
+
+    geometry.attributes.position.setZ(i, anim1 + anim2 + anim3);
   }
+
+  geometry.computeVertexNormals();
+  geometry.attributes.position.needsUpdate = true;
+
   requestAnimationFrame(animate);
   renderer.render(scene, camera);
 }
+
 animate();
