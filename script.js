9605628a0ccf84d700c8cc61ec4a45f1f8da3059
Adeyemo Ademola <adeyemoademola12@gmail.com>
2024-01-13 22:40:14 +0100

    Writing Scripts

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 import * as THREE from "three";
+import "./style.css";
+
+const container = document.querySelector(".three_bg");
+const loader = new THREE.TextureLoader();
+
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(
+  70,
+  window.innerWidth / window.innerHeight,
+  0.1,
+  1000
+);
+
+const renderer = new THREE.WebGL1Renderer();
+renderer.setSize(window.innerWidth, window.innerHeight);
+container.appendChild(renderer.domElement);
